bd7f2e56ebbe39924374511433c9cb124b6d75a7
Priya <priyaskg29@gmail.com>
2022-10-10 19:00:36 +0530

    Added

--- second.py
+++ second.py
@@ -1,1 +1,12 @@
-print("Hiii Pandubiiiii")
+from unicodedata import digit
+
+
+print("Hiii Pantaaaaaaa")
+num=1234
+rev=0
+
+while num!=0:
+    digit=num%10
+    rev=rev*10+digit
+    num//=10
+print(rev)
