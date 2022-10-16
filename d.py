a9f35bb887dda7648dce5e34c85e070fd85bb09e
Rashmi <gaikwadrashmi404@gmail.com>
2022-10-16 22:57:59 +0530

    csvdata

--- d.py
+++ d.py
@@ -1,3 +1,5 @@
 import pandas as pd
-dataset=pd.read_csv('C:\Users\Admin\Downloads\annual-enterprise-survey-2021-financial-year-provisional-size-bands-csv.csv')
-print(dataset)
+df = pd.read_csv("C:/Users/Admin/Downloads/annual-enterprise-survey-2021-financial-year-provisional-size-bands-csv.csv")
+print(df)
+
+
--- second.py
+++ second.py
@@ -10,4 +10,4 @@ while num!=0:
     digit=num%10
     rev=rev*10+digit
     num//=10
-print(rev)
+print(rev)  
